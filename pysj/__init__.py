2903f07bc61a2fa3b03e157efb0c294928a968df
sondreod <sondre_odegard@hotmail.com>
2021-12-29 18:58:03 +0100

    Add flit dependency and version bump

--- pysj/__init__.py
+++ pysj/__init__.py
@@ -1,6 +1,6 @@
 """ Pysj makes Python development more comfortable. This package contains utils, classes and helper
 functions I find myself reimplementing in several projects."""
-__version__ = "0.1a1"
+__version__ = "0.1a2"
 
 from .main import ExtendedJSONEncoder, ExtendedJSONDecoder, flatten, seconds
 from .crypto import sha256, sha1, md5, uuid
