8086da07ad11d680110bde280cdec5c3b97194a3
sondreod <sondre_odegard@hotmail.com>
2022-04-01 12:06:37 +0200

    Add force_mutable feature in hashing functions

--- pysj/crypto.py
+++ pysj/crypto.py
@@ -1,12 +1,16 @@
 import hashlib
+import json
 import uuid as _uuid
 from typing import Literal
 
 ALPHABET = "ABCDEFGHJKLMNPQRSTUVWXYZ" "abcdefghijkmnpqrstuvwxyz" "23456789"
 
 
-def _hash(plaintext: str, algorithm="sha256") -> str:
+def _hash(plaintext: str, algorithm="sha256", force_mutable=False) -> str:
     """Calculate hashdigest of plaintext using any algorithm in `hashlib.algorithms_guaranteed`"""
+
+    if force_mutable:
+        plaintext = __force_mutable(plaintext)
 
     if algorithm not in hashlib.algorithms_guaranteed:
         raise NotImplementedError(f"Hash algorithm {algorithm} is not implemented.")
@@ -15,19 +19,26 @@ def _hash(plaintext: str, algorithm="sha256") -> str:
     return hash_fn(plaintext.encode()).hexdigest()
 
 
-def sha256(plaintext: str) -> str:
+def sha256(plaintext: str, force_mutable=False) -> str:
     """Returns the hexdigest representation of the sha256 hash from the plaintext input"""
-    return _hash(plaintext, "sha256")
+    return _hash(plaintext, "sha256", force_mutable)
 
 
-def sha1(plaintext: str) -> str:
+def sha1(plaintext: str, force_mutable=False) -> str:
     """Returns the hexdigest representation of the sha1 hash from the plaintext input"""
-    return _hash(plaintext, "sha1")
+    return _hash(plaintext, "sha1", force_mutable)
 
 
-def md5(plaintext: str) -> str:
+def md5(plaintext: str, force_mutable=False) -> str:
     """Returns the hexdigest representation of the md5 hash from the plaintext input"""
-    return _hash(plaintext, "md5")
+    return _hash(plaintext, "md5", force_mutable)
+
+
+def __force_mutable(plaintext):
+    """Dumps mutable objects to string to make them hashable"""
+    if not isinstance(plaintext, str):
+        return json.dumps(plaintext)
+    return plaintext
 
 
 def uuid(type: Literal["alpha", "int"] = None, length: int = None) -> str:
