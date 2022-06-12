e3bdfbf8990edd298b39eb144426139643a0edde
sondreod <sondre_odegard@hotmail.com>
2022-06-12 21:19:42 +0200

    Version bump

--- pysj/__init__.py
+++ pysj/__init__.py
@@ -1,6 +1,6 @@
 """ Pysj makes Python development more comfortable, with utils, classes and helper
 functions I find myself reimplementing in several projects."""
-__version__ = "0.1a10"
+__version__ = "0.1a11"
 
 from .crypto import md5, sha1, sha256, uuid
 from .main import (ExtendedJSONDecoder, ExtendedJSONEncoder, Timer, flatten,
