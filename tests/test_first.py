e9779217d273d198bf257e7387d09f7fe3b19bac
sondreod <sondre_odegard@hotmail.com>
2023-01-14 20:23:09 +0100

    Add tests for *first* and *take*

--- tests/test_first.py
+++ tests/test_first.py
@@ -7,5 +7,6 @@ from pysj import first
 def test_first():
     assert first("ABCDE") == "A"
 
+
 def test_first_tuple():
-    assert first((1,2,3)) == 1
+    assert first((1, 2, 3)) == 1
--- tests/test_take.py
+++ tests/test_take.py
@@ -7,5 +7,6 @@ from pysj import take
 def test_take_4():
     assert list(take(4, "abcdefgh")) == list("abcd")
 
+
 def test_take_2():
     assert list(take(2, range(10))) == [0, 1]
